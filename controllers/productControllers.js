3e2554372e8da27e66be60bbd185d873a8d11c91
Anishz77 <pantaanish77@gmail.com>
2025-01-31 19:54:05 +0545

    changed

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 // Creating Product Function
 const createProduct = async (req, res) => {
-    // check incoming dat
+    // check incoming data
     console.log(req.body)
     console.log(req.files)
 
