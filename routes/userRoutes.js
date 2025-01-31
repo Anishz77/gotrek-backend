c2342acb68cace54b3b4f3fa315e71bc1266ec82
Anishz77 <pantaanish77@gmail.com>
2025-01-31 19:57:51 +0545

    user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.post('/create',userControllers.createUser);
 // login user api
 router.post('/login', userControllers.loginUser)
 
-// forgot password
+// forgot password api
 router.post('/forgot_password', userControllers.forgotPassword)
 
 //
