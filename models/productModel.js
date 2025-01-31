d00fdb7ee105a00791995b14e3b516aab725b489
Anishz77 <pantaanish77@gmail.com>
2025-01-31 19:54:40 +0545

    product controller

--- models/productModel.js
+++ models/productModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    // fields : NAME, PRICE, DESCRIPTION, IMAGE, CATEGORY
+    // fields : NAME, PRICE, DESCRIPTION, 
     productName: {
         type: String,
         required: true,
